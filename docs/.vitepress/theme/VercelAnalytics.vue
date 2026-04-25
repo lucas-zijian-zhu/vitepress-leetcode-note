97fd979143258f0de71b800b925e828b344ea19e
Lucas <zijian.zhu@foxmail.com>
2026-04-25 14:33:12 -0700

    add analytics

---
+++ docs/.vitepress/theme/VercelAnalytics.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { inject } from "@vercel/analytics";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  inject({ framework: "vitepress" });
+});
+</script>
+
+<template></template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,14 @@
 import DefaultTheme from "vitepress/theme";
 import { h } from "vue";
 import GiscusComments from "./GiscusComments.vue";
+import VercelAnalytics from "./VercelAnalytics.vue";
 import "./style.css";
 
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
+      "layout-bottom": () => h(VercelAnalytics),
       "doc-after": () => h(GiscusComments),
     });
   },
